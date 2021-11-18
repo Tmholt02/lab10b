2796a140f37e99bbd898d412a373e81928f11da9
tmholt02 <tmholt02@gmail.com>
2021-11-18 14:10:18 -0600

    Created Lab Files

---
+++ Lab10b_Act1.py
@@ -0,0 +1,15 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Thomas Holt
+# Section:      464
+# Assignment:   Lab10b Act 1
+# Date:         11/19/2021
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Lab10b_Act2.py
@@ -0,0 +1,15 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Thomas Holt
+# Section:      464
+# Assignment:   Lab10b_Act2
+# Date:         11/19/2021
+
+def main():
+    print("Hello World")
+
+
+if __name__ == "__main__":
+    main()
