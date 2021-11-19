1d4b0fb77daa655d87f8b836f6e5715256c9180e
Thomas Holt Laptop <tmholt02@gmail.com>
2021-11-19 10:40:29 -0600

    Finish Plot 1

--- Lab10b_Act2.py
+++ Lab10b_Act2.py
@@ -19,19 +19,40 @@ def main():
     lines = file_id.readlines()[1:]
     file_id.close()
 
+    # 2D array of plots
+    plots = plt.subplots(2, 2)[1]
+
     # Locate and plot each wind speed and temp
     winds = []
+    precips = []
     temps = []
+    tempsMax = []
+    tempsMin = []
     for line in lines:
         line = line.split(',')
-        winds.append(line[1])
-        temps.append(line[3])
+        winds.append(float(line[1]))
+        temps.append(float(line[3]))
 
-    plt.plot(winds, label="Wind avg, mph", color='blue')
-    plt.plot(temps, label="Temp avg, F", color='red')
+    ######################################################################################
 
+    # Create twin axis
+    ax_temp = plots[0][0]
+    ax_wind = ax_temp.twinx()
+
+    wind_line = ax_wind.plot(winds, label="Wind avg, mph", color='blue')
+    ax_wind.set_ylabel("Average Wind Speed (mph)", color='blue')
+
+    temp_line = ax_temp.plot(temps, label="Temp avg, F", color='red')
+    ax_temp.set_ylabel("Average Temperature (F)", color='red')
+
+    # Prepare the legend by taking each line and each label in lists
+    functions = temp_line + wind_line
+    labels = [function.get_label() for function in functions]
+    ax_wind.legend(functions, labels, loc=1)
+
+    # Title and display
+    plt.title("Temperature vs Wind Speed")
     plt.show()
-
 
 
 if __name__ == "__main__":
