32d7dff028ae2e7d8fe0d702fa49c1f94904e91e
Thomas Holt Laptop <tmholt02@gmail.com>
2021-11-19 10:58:51 -0600

    Plot 1 touch ups. Added xlabel and some other things

--- Lab10b_Act2.py
+++ Lab10b_Act2.py
@@ -8,36 +8,43 @@
 # Date:         11/19/2021
 
 # import dependencies
+from typing import List
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def main():
-
     # Read basic data from file
     file_id = open("CLLWeatherData-1.csv", 'r')
     lines = file_id.readlines()[1:]
     file_id.close()
 
-    # 2D array of plots
-    plots = plt.subplots(2, 2)[1]
+    # Divide the plot into two rows and two cols
+    plt.subplots(2, 2)
 
-    # Locate and plot each wind speed and temp
-    winds = []
-    precips = []
-    temps = []
-    tempsMax = []
-    tempsMin = []
+    # Locate and plot each value within the spreadsheet
+    winds: list[float] = []
+    precips: list[float] = []
+    temps: list[float] = []
+    temps_max: list[float] = []
+    temps_min: list[float] = []
     for line in lines:
         line = line.split(',')
         winds.append(float(line[1]))
+        precips.append(float(line[2]))
         temps.append(float(line[3]))
+        temps_max.append(float(line[4]))
+        temps_min.append(float(line[5]))
 
     ######################################################################################
 
-    # Create twin axis
-    ax_temp = plots[0][0]
+    # Create twin axis and set plt focus
+    ax_temp = plt.subplot(2, 2, 1)
+    plt.xlabel("Day")
     ax_wind = ax_temp.twinx()
+
+    plt.title("Temperature vs Wind Speed")
 
     wind_line = ax_wind.plot(winds, label="Wind avg, mph", color='blue')
     ax_wind.set_ylabel("Average Wind Speed (mph)", color='blue')
@@ -50,11 +57,11 @@ def main():
     labels = [function.get_label() for function in functions]
     ax_wind.legend(functions, labels, loc=1)
 
+    ######################################################################################
+
     # Title and display
-    plt.title("Temperature vs Wind Speed")
     plt.show()
 
 
 if __name__ == "__main__":
     main()
-
