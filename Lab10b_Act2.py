7260831c06f3161108561f0d310d4cec26bd806a
BuildTools <unconfigured@null.spigotmc.org>
2021-11-18 21:51:11 -0600

    Work done on plot 1

--- Lab10b_Act2.py
+++ Lab10b_Act2.py
@@ -7,8 +7,31 @@
 # Assignment:   Lab10b_Act2
 # Date:         11/19/2021
 
+# import dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def main():
-    print("Hello World")
+
+    # Read basic data from file
+    file_id = open("CLLWeatherData-1.csv", 'r')
+    lines = file_id.readlines()[1:]
+    file_id.close()
+
+    # Locate and plot each wind speed and temp
+    winds = []
+    temps = []
+    for line in lines:
+        line = line.split(',')
+        winds.append(line[1])
+        temps.append(line[3])
+
+    plt.plot(winds, label="Wind avg, mph", color='blue')
+    plt.plot(temps, label="Temp avg, F", color='red')
+
+    plt.show()
+
 
 
 if __name__ == "__main__":
