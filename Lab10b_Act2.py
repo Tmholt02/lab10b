a44371a6637ab27b433bf31ae6f738d7df1cdccc
Thomas Holt Laptop <tmholt02@gmail.com>
2021-11-19 16:16:43 -0600

    Last plot completed. There are some grammatical oddities and style inconsistencies, but I'll call it here!

--- Lab10b_Act2.py
+++ Lab10b_Act2.py
@@ -22,6 +22,7 @@ def main():
     plt.subplots(2, 2)
 
     # Locate and plot each value within the spreadsheet
+    dates: list[str] = []
     winds: list[float] = []
     precips: list[float] = []
     temps: list[float] = []
@@ -29,6 +30,7 @@ def main():
     temps_min: list[float] = []
     for line in lines:
         line = line.split(',')
+        dates.append(line[0])
         winds.append(float(line[1]))
         precips.append(float(line[2]))
         temps.append(float(line[3]))
@@ -83,11 +85,64 @@ def main():
 
     ######################################################################################
 
+    # Dictionary stores monthly data for finding averages
+    monthly_temp_data = {
+        # '<month>': ([<avg_temp_sum>, <sample_cnt>],
+        #             [<min_temp_sum>, <sample_cnt>],
+        #             [<max_temp_sum>, <sample_cnt>])
+        '1':  ([0, 0], [0, 0], [0, 0]),
+        '2':  ([0, 0], [0, 0], [0, 0]),
+        '3':  ([0, 0], [0, 0], [0, 0]),
+        '4':  ([0, 0], [0, 0], [0, 0]),
+        '5':  ([0, 0], [0, 0], [0, 0]),
+        '6':  ([0, 0], [0, 0], [0, 0]),
+        '7':  ([0, 0], [0, 0], [0, 0]),
+        '8':  ([0, 0], [0, 0], [0, 0]),
+        '9':  ([0, 0], [0, 0], [0, 0]),
+        '10': ([0, 0], [0, 0], [0, 0]),
+        '11': ([0, 0], [0, 0], [0, 0]),
+        '12': ([0, 0], [0, 0], [0, 0])
+    }
 
+    # We will loop through all days and find data for averaging
+    for day_idx in range(len(dates)):
+
+        # Identify what month this day is
+        month = dates[day_idx].split('/')[0]
+
+        # Increment sample counters
+        monthly_temp_data[month][0][1] += 1
+        monthly_temp_data[month][1][1] += 1
+        monthly_temp_data[month][2][1] += 1
+
+        # Add sample to the sum
+        monthly_temp_data[month][0][0] += temps[day_idx]
+        monthly_temp_data[month][1][0] += temps_min[day_idx]
+        monthly_temp_data[month][2][0] += temps_max[day_idx]
+
+    # Index = month and value = average monthly value for specified field
+    months = range(1, 13)
+    monthly_temps_avg = [value[0][0] / value[0][1] for value in monthly_temp_data.values()]
+    monthly_temps_min = [value[1][0] / value[1][1] for value in monthly_temp_data.values()]
+    monthly_temps_max = [value[2][0] / value[2][1] for value in monthly_temp_data.values()]
+
+    # Plot theses on the appropriate plot in nice styles with a legend
+    graph = plt.subplot(2, 2, 4)
+    graph.bar(months, monthly_temps_avg, color='green')
+    graph.plot(months, monthly_temps_min, color='blue', label='Low Temp')
+    graph.plot(months, monthly_temps_max, color='red', label='High Temp')
+    plt.xticks(months)
+    graph.legend()
+
+    # Title the graph and make labels
+    plt.title("Average Temperature by Month")
+    plt.xlabel("Month")
+    graph.set_ylabel("Average Temp (F)")
 
     ######################################################################################
 
     # Display the finished product
+    # I hope they like it
     plt.show()
 
 
